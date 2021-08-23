d8416411796616487fefb45a3d1321e8904c08d5
OVI19RUS <ovechkin19@gmail.com>
2021-08-23 10:30:29 +0300

    Adding Salting instead of simple Hashing

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
- 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app = express();
 
 app.set('view engine', 'ejs')
@@ -17,62 +18,65 @@ mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, us
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
-  });
+});
 
-  
+
 const User = new mongoose.model('User', userSchema);
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.render('home');
 })
 
-app.get('/login', function (req, res){
+app.get('/login', function (req, res) {
     res.render('login');
 })
 
-app.get('/register', function (req, res){
+app.get('/register', function (req, res) {
     res.render('register');
 })
 
-app.post('/register', function (req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+app.post('/register', function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets')
+            }
+        })
     });
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        } else {
-            res.render('secrets')
-        }
-    })
+
 })
 
-app.post('/login', function (req, res){
+app.post('/login', function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser){
-        if (err){
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
             console.log(err);
         } else {
-            if (foundUser){
-                if (foundUser.password === password){
-                    res.render('secrets');
-                } else {
-                    console.log('Incorrect password')
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
                 }
+                );
             }
         }
     })
 })
 
-
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+    port = 3000;
 }
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
